ec599b5a49f57095a3703c0b22d244457f875e74
Minhal Aftab <minhalaftabrana@gmail.com>
2023-06-19 13:14:00 +0500

    Finished Skills Page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import useMediaQuery from "./hooks/useMediaQuery";
 import Navbar from "./scenes/Navbar";
 import DotGroup from "./scenes/DotGroup";
 import Landing from "./scenes/Landing";
+import LineGradient from "./components/LineGradient";
+import MySkills from "./scenes/MySkills";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState("home");
@@ -35,6 +37,10 @@ function App() {
           selectedPage={selectedPage}
           setSelectedPage={setSelectedPage}
         />
+      </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto md:h-full">
+        <MySkills></MySkills>
       </div>
     </div>
   );
